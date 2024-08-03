67f531556445d336c22501f72ec5b02413810319
TimothyMendezFeliciano <timothy.mendez@upr.edu>
2024-08-02 22:36:23 -0400

    Propagating Errors

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::fs::File;
-use std::io::ErrorKind;
+use std::io;
+use std::io::{ErrorKind, Read};
 use log::error;
 
 fn main() {
@@ -33,4 +34,22 @@ fn main() {
             panic!("Problem opening the file: {:?}", error);
         }
     });
+
+
+}
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    let username_file_result = File::open("hello.txt");
+
+    let mut username_file = match username_file_result {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut username = String::new();
+
+    match username_file.read_to_string(&mut username) {
+        Ok(_) => Ok(username),
+        Err(r) => Err(r)
+    }
 }
